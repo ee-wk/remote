27e2920a1b66f2c04e5a0a22ab93b4f561107c21
ee-wk <872464959@qq.com>
2021-06-25 00:37:59 +0800

    提交作业

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
 import os
-from time import sleep
-
 import pytest
-
+from time import sleep
 from get_cookies import get_cookies
 
-os.system("chrome --remote-debugging-port=9222")
-# def run(tag=False):
-#     if tag:
-#         os.system("chrome --remote-debugging-port=9222")
-#         sleep(2)
-#         get_cookies()
-#     pytest.main(["-s", "-v"])
-#
-#
-# if __name__ == '__main__':
-#     run(tag=True)
+
+def run(tag=False):
+    if tag:
+        os.popen("chrome --remote-debugging-port=9222")
+        sleep(2)
+        get_cookies()
+    pytest.main(["-s", "-v"])
+
+
+if __name__ == '__main__':
+    run()
